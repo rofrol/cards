357ea332d3a83f76d915cd61fe82fdb9f6f36197
Roman Frołow <rofrol@gmail.com>
2013-11-11 15:14:00 +0000

    jqueryui sortable

--- script.js
+++ script.js
@@ -1,79 +1,93 @@
 $(document).ready(function(){
 
-	images = ['1.jpg','6.jpg','7.jpg'];
-	var cards = "";
-	for(var i = 0; i < images.length; i++) {
-		cards +=
-		'<div class="card_wrapper">'+
-			'<a class="image-popup-no-margins" href="'+images[i]+'">'+
-				'<img src="'+images[i]+'" class="card" />'+
-			'</a>'+
-			'<div class="card_buttons">'+
-				'<button class="rotate">r</button>'+
-				'<button class="zoom">z</button>'+
-			'</div>'+
-		'</div>';
-	}
-	$('#card_table').append(cards);
+    images = ['1.jpg','6.jpg','7.jpg'];
+    var cards = "";
+    for(var i = 0; i < images.length; i++) {
+      cards +=
+      '<div class="card_wrapper">'+
+      '<a href="'+images[i]+'">'+
+      '<img src="'+images[i]+'" class="card" />'+
+      '</a>'+
+      '<div class="card_buttons">'+
+      '<button class="rotate">r</button>'+
+      '<button class="zoom">z</button>'+
+      '</div>'+
+      '</div>';
+    }
+    $('#hand').append(cards);
 
-	//disable normal click on a surrounding card image
-	$('#card_table').on('click', '.card_wrapper a', function(e){
-		e.preventDefault();
-	});
+    //disable normal click on a surrounding card image
+    $('#card_table').on('click', '.card_wrapper a', function(e){
+      e.preventDefault();
+    });
 
-	$('#card_table').on('click', '.rotate', function(){
-		var card = $(this).closest('.card_wrapper').find('.card');
-		card.toggleClass('rotated');
-	});
+    $('#card_table').on('click', '.rotate', function(){
+        var card = $(this).closest('.card_wrapper').find('.card');
+        card.toggleClass('rotated');
+    });
 
-	$('#card_table').on('click', '.zoom',  function(e) {
-		// http://webdesign.tutsplus.com/tutorials/htmlcss-tutorials/super-simple-lightbox-with-css-and-jquery/
-		// http://www.net-kit.com/10-css3-lightbox-alternatives/
-		// http://lokeshdhakar.com/projects/lightbox2/
-		// http://dimsemenov.com/plugins/magnific-popup/
-		// http://brutaldesign.github.io/swipebox/
+    $('#card_table').on('click', '.zoom',  function(e) {
+        // http://webdesign.tutsplus.com/tutorials/htmlcss-tutorials/super-simple-lightbox-with-css-and-jquery/
+        // http://www.net-kit.com/10-css3-lightbox-alternatives/
+        // http://lokeshdhakar.com/projects/lightbox2/
+        // http://dimsemenov.com/plugins/magnific-popup/
+        // http://brutaldesign.github.io/swipebox/
 
-		//prevent default action (hyperlink)
+        //prevent default action (hyperlink)
 
-		//Get clicked link href
-		var image_href = $(this).closest('.card_wrapper').find('.card').attr("src");
+        //Get clicked link href
+        var image_href = $(this).closest('.card_wrapper').find('.card').attr("src");
 
-		/*
-		If the lightbox window HTML already exists in document,
-		change the img src to to match the href of whatever link was clicked
+        /*
+           If the lightbox window HTML already exists in document,
+           change the img src to to match the href of whatever link was clicked
 
-		If the lightbox window HTML doesn't exists, create it and insert it.
-		(This will only happen the first time around)
-		*/
+           If the lightbox window HTML doesn't exists, create it and insert it.
+           (This will only happen the first time around)
+           */
 
-		if ($('#lightbox').length > 0) { // #lightbox exists
+        if ($('#lightbox').length > 0) { // #lightbox exists
 
-			//place href as img src value
-			$('#content').html('<img src="' + image_href + '" />');
+          //place href as img src value
+          $('#content').html('<img src="' + image_href + '" />');
 
-			//show lightbox window - you could use .show('fast') for a transition
-			$('#lightbox').show();
-		} else { //#lightbox does not exist - create and insert (runs 1st time only)
-			//create HTML markup for lightbox window
-			var lightbox =
-			'<div id="lightbox">' +
-					'<img src="' + image_href +'" />' +
-			'</div>';
+          //show lightbox window - you could use .show('fast') for a transition
+          $('#lightbox').show();
+        } else { //#lightbox does not exist - create and insert (runs 1st time only)
+          //create HTML markup for lightbox window
+          var lightbox =
+            '<div id="lightbox">' +
+            '<img src="' + image_href +'" />' +
+            '</div>';
 
-			//insert lightbox HTML into page
-			$('body').append(lightbox);
-		}
-	});
+          //insert lightbox HTML into page
+          $('body').append(lightbox);
+        }
+    });
 
-	$(document).on('click', '#lightbox', function(){
-		$('#lightbox').hide();
-	});
-
-	$(function() {
-		$('.card_wrapper').draggable({
-            stack: '.card_wrapper',
-			cursor: 'move',        // sets the cursor apperance
-			containment: '#card_table'    // sets to can be dragged only within "#drg" element
+    $(document).on('click', '#lightbox', function(){
+        $('#lightbox').hide();
+    });
+/*
+    $(function() {
+        $('.card_wrapper').draggable({
+          stack: '.card_wrapper',
+          cursor: 'move',        // sets the cursor apperance
+          containment: '#card_table'    // sets to can be dragged only within "#drg" element
         });
-	});
+    });
+*/
+
+    $('#makeMeDroppable').droppable( {
+      drop: handleDropEvent
+    });
+
+    function handleDropEvent( event, ui ) {
+      var draggable = ui.draggable;
+      ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+    }
+
+    $("#hand").sortable({connectWith: ".drop"});
+    $(".drop").sortable({connectWith: "#hand"});
+    
 });
